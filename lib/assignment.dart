c332164830815334e87bb1f996d31609b1642c85
hudaif99 <hudaifpaladan99@gmail.com>
2021-12-24 10:41:47 +0530

    created shapes and aligned them in column

--- lib/assignment.dart
+++ lib/assignment.dart
@@ -12,102 +12,122 @@ class _thirdPageState extends State<thirdPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-          Container(
-            width: 150,
-            height: 150,
-            decoration: BoxDecoration(
-                color: Colors.yellow,
-                shape: BoxShape.circle,
-                border: Border.all(
-                  color: Colors.black,
-                  width: 3,
-                )),
-            child: Center(child: Text("1")),
-          ),
-          Container(
-            width: 150,
-            height: 150,
-            decoration: BoxDecoration(
-                color: Colors.yellow,
-                shape: BoxShape.circle,
-                border: Border.all(
-                  color: Colors.black,
-                  width: 3,
-                )),
-            child: Center(child: Text("2")),
-          ),
-          Container(
-            width: 150,
-            height: 150,
-            decoration: BoxDecoration(
-                color: Colors.yellow,
-                shape: BoxShape.circle,
-                border: Border.all(
-                  color: Colors.black,
-                  width: 3,
-                )),
-            child: Center(child: Text("3")),
-          ),
-          //creating rectangles
+        body: Column(
+          children: [
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 150,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 3,
+                        )),
+                    child: Center(child: Text("1")),
+                  ),
+                  Container(
+                    width: 150,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 3,
+                        )),
+                    child: Center(child: Text("2")),
+                  ),
+                  Container(
+                    width: 150,
+                    height: 150,
+                    decoration: BoxDecoration(
+                        color: Colors.yellow,
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 3,
+                        )),
+                    child: Center(child: Text("3")),
+                  ),
+                  //creating rectangles
 
-          Container(
-            child: Row(
+
+                  //creating bottom circle
+
+
+                ]),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 200,
+                        height: 120,
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            shape: BoxShape.rectangle,
+                            border: Border.all(
+                              color: Colors.black,
+                              width: 3,
+                            )),
+                        child: Center(child: Text("4")),
+                      ),
+                      Container(
+                        width: 200,
+                        height: 120,
+                        decoration: BoxDecoration(
+                            color: Colors.red,
+                            shape: BoxShape.rectangle,
+                            border: Border.all(
+                              color: Colors.black,
+                              width: 3,
+                            )),
+                        child: Center(child: Text("5")),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Container(
-                  width: 200,
-                  height: 120,
-                  decoration: BoxDecoration(
-                      color: Colors.red,
-                      shape: BoxShape.rectangle,
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 3,
-                      )),
-                  child: Center(child: Text("4")),
-                ),
-                Container(
-                  width: 200,
-                  height: 120,
-                  decoration: BoxDecoration(
-                      color: Colors.red,
-                      shape: BoxShape.rectangle,
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 3,
-                      )),
-                  child: Center(child: Text("5")),
-                )
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 150,
+                          height: 150,
+                          decoration: BoxDecoration(
+                            color: Colors.yellow,
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: Colors.black,
+                              width: 3,
+                            ),
+                          ),
+                          child: Center(child: Text("6")),
+                        )
+                      ],
+                    ))
               ],
-            ),
-          ),
-          //creating bottom circle
-
-          Container(
-              child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                width: 150,
-                height: 150,
-                decoration: BoxDecoration(
-                  color: Colors.yellow,
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    color: Colors.black,
-                    width: 3,
-                  ),
-                ),
-                child: Center(child: Text("6")),
-              )
-            ],
-          ))
-        ]));
+            )
+          ],
+        )
+    );
   }
 }
